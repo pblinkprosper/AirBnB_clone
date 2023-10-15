94bc812abea0b97edd3214c4479085b19e7ed958
Rosemarynwodo <rn.nwodo@gmail.com>
2023-10-15 17:27:07 +0100

    Complete model package updated

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Review"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents a review"""
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state"""
+    name = ""
